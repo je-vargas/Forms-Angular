9b2030d1b1bc46bb24990054fe05a48fc0cf3853
juan-vargas-salamanca <juan.vargas-salamanca@landmark.co.uk>
2020-08-11 22:49:06 +0100

    method added to component called when sending form

--- My-First-Form/src/app/user-settings-form/user-settings-form.component.ts
+++ My-First-Form/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,26 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
-  styleUrls: ['./user-settings-form.component.css']
+  styleUrls: ['./user-settings-form.component.css'],
 })
-export class UserSettingsFormComponent implements OnInit {
 
+export class UserSettingsFormComponent implements OnInit {
   originalSetting: UserSettings = {
     name: 'Milton',
     emailOffers: true,
     interfaceStyle: 'dark',
     subscriptionType: 'Annual',
-    notes: 'here are some notes...'
+    notes: 'here are some notes...',
   };
 
-  userSetting: UserSettings = {...this.originalSetting}
+  userSetting: UserSettings = { ...this.originalSetting };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubbmit: ', form.valid);
   }
-
 }
